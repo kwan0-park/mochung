247374f01b6ceb199240f6f7392252c26a2221b3
kwan0-park <e8d3o7di@naver.com>
2025-10-06 19:09:32 +0900

    feat: 네이버지도, 카카오지도 추가
    Signed-off-by: kwan0-park <e8d3o7di@naver.com>

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:map_launcher/map_launcher.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -9,45 +8,33 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
-  _launchMaps() async {
-    final availableMaps = await MapLauncher.installedMaps;
-    final destination = Coords(37.515488, 127.064848);
-    const destinationTitle = "노블발렌티 삼성점";
+  _launchNaverMap() async {
+    const query = "노블발렌티 삼성점";
+    final encodedQuery = Uri.encodeComponent(query);
+    const appname = "com.example.wedding_invitation";
+    final naverMapAppUrl = Uri.parse('nmap://search?query=$encodedQuery&appname=$appname');
+    final naverMapWebUrl = Uri.parse('https://map.naver.com/v5/search/$encodedQuery');
 
-    if (availableMaps.length > 1) {
-      showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return SafeArea(
-            child: SingleChildScrollView(
-              child: Container(
-                child: Wrap(
-                  children: <Widget>[
-                    for (var map in availableMaps)
-                      ListTile(
-                        onTap: () => map.showDirections(
-                          destination: destination,
-                          destinationTitle: destinationTitle,
-                        ),
-                        title: Text(map.mapName),
-                        leading: SvgPicture.asset(
-                          map.icon,
-                          height: 30.0,
-                          width: 30.0,
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      );
-    } else if (availableMaps.length == 1) {
-      await availableMaps.first.showDirections(
-        destination: destination,
-        destinationTitle: destinationTitle,
-      );
+    if (await canLaunchUrl(naverMapAppUrl)) {
+      await launchUrl(naverMapAppUrl);
+    } else {
+      await launchUrl(naverMapWebUrl, mode: LaunchMode.externalApplication);
+    }
+  }
+
+  _launchKakaoMap() async {
+    const query = "노블발렌티 삼성점";
+    const lat = 37.515488;
+    const lng = 127.064848;
+    final encodedQuery = Uri.encodeComponent(query);
+
+    final kakaoMapAppUrl = Uri.parse('kakaomap://search?q=$encodedQuery&p=$lat,$lng');
+    final kakaoMapWebUrl = Uri.parse('http://m.map.kakao.com/scheme/search?q=$encodedQuery&p=$lat,$lng');
+
+    if (await canLaunchUrl(kakaoMapAppUrl)) {
+      await launchUrl(kakaoMapAppUrl);
+    } else {
+      await launchUrl(kakaoMapWebUrl, mode: LaunchMode.externalApplication);
     }
   }
 
@@ -59,17 +46,45 @@ class _MapState extends State<Map> {
         child: Column(
           children: [
             const SizedBox(height: 12),
-            ClipRRect (
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/images/map.webp',
-                fit: BoxFit.cover,
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  width: 0.25,
+                  color: const Color.fromRGBO(41, 82, 56, 1.0)
+                ),
+              ),
+              child: ClipRRect (
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/images/map.webp',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: _launchMaps,
-              child: const Text('지도로 길 찾기'),
+            Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: [
+                ElevatedButton(
+                  onPressed: _launchNaverMap,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text('네이버 지도'),
+                ),
+                ElevatedButton(
+                  onPressed: _launchKakaoMap,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text('카카오맵'),
+                ),
+              ],
             ),
             const SizedBox(height: 12),
             const Text(
@@ -85,11 +100,11 @@ class _MapState extends State<Map> {
               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, height: 3)
             ),
             const Text(
-              '- 지하철: 9호선 봉은사역 4번 출구 도보 5분\n'
-              '- 버스: 봉은사역 3번 출구, 삼성 1파출소 정류장\n'
-              '  지선(초록): 2413, 2415, 3217, 3414, 4318\n'
-              '  간선(파랑): 143, 146, 301, 343, 345, 401\n'
-              '  직행(빨강): 9407, 9507, 9607\n'
+              '- 지하철: 9호선 봉은사역 4번 출구 도보 5분\n' 
+              '- 버스: 봉은사역 3번 출구, 삼성 1파출소 정류장\n' 
+              '  지선(초록): 2413, 2415, 3217, 3414, 4318\n' 
+              '  간선(파랑): 143, 146, 301, 343, 345, 401\n' 
+              '  직행(빨강): 9407, 9507, 9607\n' 
               '- 셔틀 운행: 봉은사역 5번 출구, 제2주차장 (5분 간격)',
               style: TextStyle(height: 2,)
             ),
@@ -99,7 +114,7 @@ class _MapState extends State<Map> {
               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, height: 3)
             ),
             const Text(
-              '본관 및 제2주차장 이용 가능\n'
+              '본관 및 제2주차장 이용 가능\n' 
               '(2시간 무료, 제2주차장 도보 4분)',
               style: TextStyle(height: 2,)
             ),
