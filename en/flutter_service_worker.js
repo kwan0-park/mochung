6c98fc38840c7fda951c63cea76ea3916978139d
kwan0-park <kwan0-park@users.noreply.github.com>
2025-12-23 14:54:26 +0000

    deploy: dde33d0eee74b47b3bebda0656df1f669f8baefa

--- en/flutter_service_worker.js
+++ en/flutter_service_worker.js
@@ -8,8 +8,8 @@ const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "icons/web-app-manifest-192x192.png": "a2de5abadab2c5c7fe05233c5506be85",
 "icons/apple-touch-icon.png": "2396bd519e01545ca2b47e969ccf51d0",
 "manifest.json": "11aba76632318e2dbfcdecaa1807ab52",
-"index.html": "b2aed7663e28ec98cff2b301ea806769",
-"/": "b2aed7663e28ec98cff2b301ea806769",
+"index.html": "7ae5fbc7e114a3d8791b1adb798633a8",
+"/": "7ae5fbc7e114a3d8791b1adb798633a8",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "41d7d96278e8dbd25848e63424db92b5",
 "assets/assets/images/invitation_otter_cropped.png": "048e52a404c12f33a9e33843c5ac24df",
@@ -82,9 +82,9 @@ const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "canvaskit/skwasm.js.symbols": "c3c05bd50bdf59da8626bbe446ce65a3",
 "favicon.png": "e2253c006e72b2a1d8f600550f963fe9",
 "CNAME": "a69bc9fdd2f911f8518cf4b6a5c1d985",
-"flutter_bootstrap.js": "7fac97476c61f009a44003cd5d062111",
+"flutter_bootstrap.js": "8249d14103cb703413f3eb3ee45f9628",
 "version.json": "fa62a0f385fd246e09c15612da75b2b8",
-"main.dart.js": "8d1add19fc6119a0f0d2cb9a0db65209"};
+"main.dart.js": "e0a55bb34b5bd53cca9df8c298968624"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
